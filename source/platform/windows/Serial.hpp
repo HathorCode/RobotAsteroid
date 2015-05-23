52a7f12276e519e3ba70e05c788fd2e3f908c8cc
Corey Wieser <worey1@gmail.com>
2015-05-23 15:25:48 -0400

    Serial access should be finished and working.

--- source/platform/windows/Serial.hpp
+++ source/platform/windows/Serial.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <Windows.h>
+#include <assert.h>
+#include <vector>
 
 namespace robitRabit {
 	struct Serial {
 		DCB dcb;
+		DWORD bytesWritten;
 		HANDLE dataHandle;
 
 		void Init() {
@@ -15,12 +18,23 @@ namespace robitRabit {
 			dcb.ByteSize = 8;
 			dcb.Parity = NOPARITY;
 			dcb.StopBits = ONESTOPBIT;
+			assert(!SetCommState(dataHandle, &dcb));
 		}
 
-		void SendData() {
-			if (!SetCommState(dataHandle, &dcb)) {
+		inline void SendData(char* dataToSend) {
+			bool writtenFile = WriteFile(dataHandle, dataToSend, 6, &bytesWritten, NULL);
+		}
 
+		/* Should be the only function used to send instructions to the Carduino. Input the array of characters from the generated path*/
+		void SendInstructionSet(std::vector<char*> instructionVector) {
+			bool bufferSize = WriteFile(dataHandle, (LPCVOID)instructionVector.size(), sizeof(instructionVector.size()), &bytesWritten, NULL);
+			for (int indexOfBuffer = 0; indexOfBuffer < instructionVector.size(); indexOfBuffer++) {
+				SendData(instructionVector[indexOfBuffer]);
 			}
 		}
-	};
+
+		void Close() {
+			CloseHandle(dataHandle);
+		}
+	} serial;
 }
